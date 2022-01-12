ae0361167a7d61589879643acfc376b03eb9a873
RohithReddy20 <rohithreddyrohi18@gmail.com>
2022-01-12 12:31:23 +0530

    deploy

---
+++ src/components/pages/AnnouncementsPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announcements from '../Announcements/Announcements'
+import Navbar from '../Navbar/Navbar'
+import Sidebar from '../Sidebar/Sidebar'
+
+function AnnouncementsPage() {
+    return (
+        <>
+            <Navbar />
+            <Sidebar />
+            <Announcements />
+        </>
+    )
+}
+
+export default AnnouncementsPage
